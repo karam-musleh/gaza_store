5da3e871ef234f18112c97450e91394935119946
karam-musleh <kk420271025@gmail.com>
2025-01-07 14:50:15 +0200

    dashborde

---
+++ lang/en/admin.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return [
+    'dash'=> 'Dashboard',
+    'categories'=>'Categories',
+    'all_categories'=>'All Categories',
+
+    'product'=>'Product',
+    'all_product'=>'All Product',
+
+    'role'=>'Role',
+    'all_role'=>'All Role',
+
+    'orders'=>'Orders',
+    'payment'=>'Payment-',
+
+    'customers'=>'Customers',
+    'add_new'=>'Add New'
+];
+//
---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+// Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+Route::prefix(LaravelLocalization::setLocale())->group(function(){
+    Route::prefix('admin')->name('admin.')->group(function (){
+        Route::get('/',[AdminController::class ,'index'])->name('index');
+
+});
+});
+//
